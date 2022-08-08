71261ed6e693f3cd834cfa14daf16269dd8f28c7
Philipp Petry <pp.petry@outlook.de>
2022-08-08 16:49:04 +0200

    cable ripping update

--- Assets/Scripts/Philipp/CableRipping.cs
+++ Assets/Scripts/Philipp/CableRipping.cs
@@ -19,7 +19,15 @@ public class CableRipping : MonoBehaviour
     [SerializeField] float minDistance= 0.0f;
     [SerializeField] float maxDistance = 0.01f;
 
-    private class CableEnds
+    private class r_CableEnds
+    {
+        public GameObject gmObj;
+        public GameObject startingPoint;
+        public float breakForce;
+        public bool isConnected = true;
+    }
+
+    private class l_CableEnds
     {
         public GameObject gmObj;
         public GameObject startingPoint;
@@ -30,13 +38,13 @@ public class CableRipping : MonoBehaviour
     List<GameObject> r_cableStartObjects = new List<GameObject>();
     private GameObject[] r_cableStartPositionAR;
 
-    List<CableEnds> r_cableEndObjects = new List<CableEnds>();
+    List<r_CableEnds> r_cableEndObjects = new List<r_CableEnds>();
     private GameObject[] r_cableEndPositionAR;
 
     List<GameObject> l_cableStartObjects = new List<GameObject>();
     private GameObject[] l_cableStartPositionAR;
 
-    List<CableEnds> l_cableEndObjects = new List<CableEnds>();
+    List<l_CableEnds> l_cableEndObjects = new List<l_CableEnds>();
     private GameObject[] l_cableEndPositionAR;
 
 
@@ -51,12 +59,12 @@ public class CableRipping : MonoBehaviour
 
         for (int i = 0; i < r_cableEndPositionAR.Length; i++)
         {
-            r_cableEndObjects.Add(new CableEnds { gmObj = r_cableEndPositionAR[i], breakForce = Random.Range(cableResistanceMin, cableResistanceMax) });
+            r_cableEndObjects.Add(new r_CableEnds { gmObj = r_cableEndPositionAR[i], breakForce = Random.Range(cableResistanceMin, cableResistanceMax) });
         }
 
         for (int i = 0; i < l_cableEndPositionAR.Length; i++)
         {
-            l_cableEndObjects.Add(new CableEnds { gmObj = l_cableEndPositionAR[i], breakForce = Random.Range(cableResistanceMin, cableResistanceMax) });
+            l_cableEndObjects.Add(new l_CableEnds { gmObj = l_cableEndPositionAR[i], breakForce = Random.Range(cableResistanceMin, cableResistanceMax) });
         }
 
         for (int i = 0; i < r_cableStartPositionAR.Length; i++)
@@ -119,24 +127,47 @@ public class CableRipping : MonoBehaviour
             Debug.Log("Right Velocity = " + r_veloX);
         }
 
-        for (int i = 0; i < r_cableEndObjects.Count; i++)
-        {
-            if (/*cableEndObjects[i].isConnected || */ r_veloX > r_cableEndObjects[i].breakForce)
-            {
-                DetachCable(r_cableEndObjects[i]);
-            }
-        }
-
         for (int i = 0; i < l_cableEndObjects.Count; i++)
         {
             if (l_veloX > l_cableEndObjects[i].breakForce)
             {
-                DetachCable(l_cableEndObjects[i]);
+                l_DetachCable(l_cableEndObjects[i]);
             }
+        }
+ 
+        for (int i = 0; i < r_cableEndObjects.Count; i++)
+        {
+            if (r_veloX > r_cableEndObjects[i].breakForce)
+            {
+                r_DetachCable(r_cableEndObjects[i]);
+            }
+        }
+
+    }
+
+    private void r_DetachCable(r_CableEnds item)
+    {
+        item.gmObj.transform.SetParent(item.startingPoint.transform);
+        item.startingPoint.AddComponent<SpringJoint>();
+
+        var springJoint = item.startingPoint.GetComponent<SpringJoint>();
+        springJoint.connectedBody = item.gmObj.GetComponent<Rigidbody>();
+        springJoint.autoConfigureConnectedAnchor = false;
+        springJoint.spring = spring;
+        springJoint.damper = damper;
+        springJoint.maxDistance = maxDistance;
+        springJoint.minDistance = minDistance;
+        item.gmObj.GetComponent<BoxCollider>().isTrigger = false;
+        item.gmObj.GetComponent<Rigidbody>().useGravity = true;
+        item.isConnected = false;
+
+        if (Use_Debug_Statements)
+        {
+            Debug.Log("Cable Detached: " + item.gmObj.name);
         }
     }
 
-    private void DetachCable(CableEnds item)
+    private void l_DetachCable(l_CableEnds item)
     {
         item.gmObj.transform.SetParent(item.startingPoint.transform);
         item.startingPoint.AddComponent<SpringJoint>();
