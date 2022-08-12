ade933692d158aa284c3fd9d7db6bf887bba0173
Torben Storch <glo_age@hotmail.com>
2022-08-12 19:29:06 +0200

    + adjustments

--- Assets/ManageVoice.cs
+++ Assets/ManageVoice.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class ManageVoice : MonoBehaviour
 {
     private AudioSource _audioSource;
+    [SerializeField] GameObject door;
+	[SerializeField] Quaternion doorNew;
+	[SerializeField] float steps;
 
-    void Start()
+	void Start()
     {
         _audioSource = GetComponent<AudioSource>();
     }
@@ -21,6 +24,23 @@ public class ManageVoice : MonoBehaviour
         yield return new WaitForSeconds(15.0f);
 
         _audioSource.Play();
+
+        yield return new WaitForSeconds(9f);
+        StartCoroutine(DoorMove());
+       
+    }
+
+    IEnumerator DoorMove()
+	{
+        float f = 0f;
+        while (f < 1)
+		{
+            f += steps;
+            door.transform.rotation = Quaternion.Lerp(door.transform.rotation, doorNew, f);
+            yield return null;
+        }
+        door.transform.rotation = doorNew;
+        yield return null;
     }
 
 }
