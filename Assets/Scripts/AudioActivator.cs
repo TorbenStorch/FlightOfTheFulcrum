b670a8260222b3d14552794463ebc794d94181e4
Philipp Petry <pp.petry@outlook.de>
2022-09-20 16:16:34 +0200

    audio fix

---
+++ Assets/Scripts/AudioActivator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioActivator : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+    [SerializeField] GameObject triggerObject;
+    [SerializeField] float delayTimeInSec;
+
+    public UnityEvent triggerDelay;
+    public bool audioListening { set; get; }
+    private bool hasPlayed = false;
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == triggerObject.name && audioListening && !hasPlayed)
+        {
+            audio.Play();
+            hasPlayed = true;
+            Invoke("TriggerEnterDelay", delayTimeInSec);
+            Debug.Log("Introduction Audio Played");
+        }
+    }
+
+    private void TriggerEnterDelay()
+    {
+        triggerDelay.Invoke();
+    }
+}
